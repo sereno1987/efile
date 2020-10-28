980ec0fe81d1e38eb82a1616cbada53ef8182037
saba <sereno.project@gmail.com>
2020-10-28 11:49:07 +0330

    adding old value to validations

--- resources/views/admin/user/form.blade.php
+++ resources/views/admin/user/form.blade.php
@@ -5,12 +5,12 @@
         <form action="" method="post">
             {{csrf_field()}}
             <div class="form-group">
-                <label for="firstName">نام کامل</label>
-                <input type="text" name="name"  class="form-control" id="firstName" placeholder="نام و فامیلی خود را به صورت کامل و با حروف فارسی وارد کنید. ">
+                <label for="name">نام کامل</label>
+                <input type="text" name="name"  class="form-control" id="name" value="{{old('name')}}" placeholder="نام و فامیلی خود را به صورت کامل و با حروف فارسی وارد کنید. ">
             </div>
             <div class="form-group">
                 <label for="email">آدرس ایمیل </label>
-                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" value="{{old('email')}}">
                 <small id="emailHelp" class="form-text text-muted">email@example.com</small>
             </div>
             <div class="form-group">
@@ -21,7 +21,7 @@
 
             <div class="form-group">
                 <label for="role">نقش کاربر</label>
-                <select  class="form-control" name="role" id="role">
+                <select  class="form-control" name="role" id="role" value="{{old('name')}}">
                     <option value=1>کاربر عادی</option>
                     <option value=2> اپراتور</option>
                     <option value=3>مدیر</option>
@@ -29,7 +29,7 @@
             </div>
             <div class="form-group">
                 <label for="wallet">موجودی کیف پول</label>
-                <input type="text" name="wallet" class="form-control" id="wallet" value=0>
+                <input type="text" name="wallet" class="form-control" id="wallet" value="{{old('wallet')}}">
             </div>
 
                 <div class="form-group">
